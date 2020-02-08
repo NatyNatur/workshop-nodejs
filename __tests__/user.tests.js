aebfe48297935934454fecbf820ca61f8a7ac43e
NatyNatur <naticcg@gmail.com>
2020-02-08 16:23:53 -0300

    second commit

---
+++ __tests__/user.tests.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+import app from '../src/server';
+
+describe('Create user', () => {
+  beforeAll(async () => {
+    await mongoose
+      .connect(process.env.MONGODB_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      });
+  });
+
+  it('Should get all users', async (done) => {
+    const res = await supertest(app)
+      .get('/users');
+
+    expect(res.statusCode).toEqual(200);
+    done();
+  });
+
+  it('Should create new user', async (done) => {
+    const res = await supertest(app)
+      .post('/users')
+      .send({
+        name: 'test 1',
+        email: 'email test 1',
+      });
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body.message).toEqual('Usuario creado exitosamente');
+    done();
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
