cb3a6479043b922bb1753753ccd2ce105e395ce6
NatyNatur <naticcg@gmail.com>
2020-02-08 14:00:58 -0300

    first commit

---
+++ src/controllers/index.js
@@ -0,0 +1,8 @@
+import usersController from './usersController';
+import postsControllers from './postControllers';
+import commentsControllers from './commentsControllers';
+export {
+    usersController,
+    postsControllers,
+    commentsControllers,
+};
---
+++ src/controllers/usersController.js
@@ -0,0 +1,23 @@
+const list = (req, res) => {
+    return res.send({ message: 'Soy el controlador del usuario' })
+}
+const find = (req, res) => {
+    return res.send({ message: 'Soy el controlador del usuario' })
+}
+const create = (req, res) => {
+    return res.send({ message: 'Soy el controlador del usuario' })
+}
+const update = (req, res) => {
+    return res.send({ message: 'Soy el controlador del usuario' })
+}
+const del = (req, res) => {
+    return res.send({ message: 'Soy el controlador del usuario' })
+}
+
+export default {
+    list,
+    find,
+    create,
+    update,
+    del
+}
